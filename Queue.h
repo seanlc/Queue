53b58148249f32795084d0fabf9385a92084cc33
Sean <seanlcarstensen@gmail.com>
2017-07-03 19:01:04 -0700

    constructors and dtors done

--- Queue.h
+++ Queue.h
@@ -1,11 +1,23 @@
 class Queue {
   public:
-    Queue();
-    Queue(int cap);
-    ~Queue();
+    Queue()
+    : buf (new int [500]), back (0), front (0)
+    {}
+    Queue(int cap)
+    : buf (new int [cap] ), back (0), front (0)
+    {}
+    ~Queue()
+    {
+        delete [] buf;
+	buf = nullptr;
+    }
     void enqueue(int el);
     int dequeue();
     int peek();
     bool isEmpty();
     void clear();
+  private:
+    int * buf;
+    int back;
+    int front;
 };
--- makefile
+++ makefile
@@ -5,5 +5,7 @@ main: Queue.h.gch
 	$(CC) $(CFLAGS) main.cpp -o main
 Queue.h.gch: Queue.h
 	$(CC) $(CFLAGS) Queue.h
+valgrind:
+	valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --track-origins=yes ./main
 clean:
 	rm -f main Queue.h.gch
